f28427a7d23bf6f6c31a692f3c8df32d26ae3434
揭思敏 <jsm0834@175game.com>
2018-06-19 23:33:16 +0800

    003-二维数组中的查找: v2: refactor: 使用二分查找、找到结果后提前终止循环
    由于每行的数据是升序的，所以可以使用二分查找从而提高效率。
    "func(i int) bool { return row[i] >= number }" 表示要查找的是升序序列。
    
    找到结果后可以提前终止循环。

---
+++ 003-二维数组中的查找/v2/search.go
@@ -0,0 +1,15 @@
+package main
+
+import "sort"
+
+func Search(array [][]int, number int) (result bool) {
+	for _, row := range array {
+		i := sort.Search(len(row), func(i int) bool { return row[i] >= number })
+		if i < len(row) {
+			result = true
+			break
+		}
+	}
+
+	return
+}

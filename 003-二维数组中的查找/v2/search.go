3087552ca10dc6197860fd88f30066128042d8ab
揭思敏 <jsm0834@175game.com>
2018-06-19 23:48:37 +0800

    003-二维数组中的查找: v2: fix: sort.Search 还要判断是否相等

--- 003-二维数组中的查找/v2/search.go
+++ 003-二维数组中的查找/v2/search.go
@@ -5,7 +5,7 @@ import "sort"
 func Search(array [][]int, number int) (result bool) {
 	for _, row := range array {
 		i := sort.Search(len(row), func(i int) bool { return row[i] >= number })
-		if i < len(row) {
+		if i < len(row) && row[i] == number {
 			result = true
 			break
 		}

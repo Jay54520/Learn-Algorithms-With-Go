3fdf84b2985632ecd6c43893852695404c13baf0
揭思敏 <jsm0834@175game.com>
2018-06-19 23:16:30 +0800

    003-二维数组中的查找: v1: fix: got true want false

--- 003-二维数组中的查找/v1/search.go
+++ 003-二维数组中的查找/v1/search.go
@@ -1,6 +1,13 @@
 package main
 
 func Search(array [][]int, number int) (result bool) {
-	result = true
+	for _, row := range array {
+		for _, n := range row {
+			if number == n {
+				result = true
+			}
+		}
+	}
+
 	return
 }

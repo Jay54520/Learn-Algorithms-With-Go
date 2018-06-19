025e397be9de613360aafc08c16408bc3898dacb
揭思敏 <jsm0834@175game.com>
2018-06-19 23:06:16 +0800

    003-二维数组中的查找: v1: test: add TestSearch

---
+++ 003-二维数组中的查找/v1/search_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestSearch(t *testing.T)  {
+
+	array := [][]int{
+		{1, 2, 8, 9},
+		{2, 4, 9, 12},
+		{4, 7, 10, 13},
+		{6, 8, 11, 15},
+	}
+
+	t.Run("查找存在的数字", func(t *testing.T) {
+		got := Search(array, 5)
+		want := 5
+
+		if want != got {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+
+}

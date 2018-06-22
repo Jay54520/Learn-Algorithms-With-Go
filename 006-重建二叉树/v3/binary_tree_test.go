1a882dc3ae9b9992868e43dd41cc693ee56a8fd2
揭思敏 <jsm0834@175game.com>
2018-06-22 09:22:16 +0800

    006-重建二叉树: v3: feat: 增加 in order traversal
    与 pre order traversal 的区别是将取出根节点的代码移动到遍历左右子树之间。

---
+++ 006-重建二叉树/v3/binary_tree_test.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"testing"
+	"reflect"
+)
+
+func TestBinaryTree(t *testing.T)  {
+	level := Node{
+		nil,
+		nil,
+		0,
+	}
+	left := Node{
+		nil,
+		nil,
+		1,
+	}
+	right := Node{
+		nil,
+		nil,
+		2,
+	}
+	root := Node{
+		&left,
+		&right,
+		0,
+	}
+
+	t.Run("level node", func(t *testing.T) {
+		if level.left != nil {
+			t.Errorf("%v doesn't have left node", level)
+		}
+		if level.right != nil {
+			t.Errorf("%v doesn't have right node", level)
+		}
+		if level.value != 0 {
+			t.Errorf("got %v want %v", level.value, 0)
+		}
+	})
+	
+	t.Run("three nodes binary tree", func(t *testing.T) {
+		if root.left != &left {
+			t.Errorf("got %v want %v", root.left, &left)
+		}
+		if root.right != &right {
+			t.Errorf("got %v want %v", root.right, &right)
+		}
+		if root.value != 0 {
+			t.Errorf("got %v want %v", root.value, 0)
+		}
+	})
+
+	t.Run("pre order traversal", func(t *testing.T) {
+		var got []int
+		got = root.PreOrderTraversal(got)
+		want := []int{0, 1, 2}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("pre order traversal with one node", func(t *testing.T) {
+		var got []int
+		got = level.PreOrderTraversal(got)
+		want := []int{0}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("in order traversal", func(t *testing.T) {
+		var got []int
+		got = root.InOrderTraversal(got)
+		want := []int{1, 0, 2}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+}
---
+++ 006-重建二叉树/v3/node.go
@@ -0,0 +1,37 @@
+package main
+
+type Node struct {
+	left  *Node
+	right *Node
+	value int
+}
+
+func (node *Node) PreOrderTraversal(result []int) []int {
+	if node != nil {
+		result = append(result, node.value)
+	}
+	if node.left != nil {
+		result = node.left.PreOrderTraversal(result)
+	}
+	if node.right != nil {
+		result = node.right.PreOrderTraversal(result)
+	}
+
+	return result
+}
+
+
+func (node *Node) InOrderTraversal(result []int) []int {
+	if node.left != nil {
+		result = node.left.PreOrderTraversal(result)
+	}
+	if node != nil {
+		result = append(result, node.value)
+	}
+	if node.right != nil {
+		result = node.right.PreOrderTraversal(result)
+	}
+
+	return result
+
+}

80ae3702c661efb9d0550420568fd3d5b5fa68ad
揭思敏 <jsm0834@175game.com>
2018-06-26 21:24:20 +0800

    006-重建二叉树: v6: fix: pass test "four nodes tree"

--- 006-重建二叉树/v6/rebuild_binary_tree.go
+++ 006-重建二叉树/v6/rebuild_binary_tree.go
@@ -1,24 +1,67 @@
 package main
 
 func RebuildBinaryTree(preOrderTraversalResult []int, inOrderTraversalResult []int) *Node {
-	rootValue := preOrderTraversalResult[0]
-	leftValue := inOrderTraversalResult[0]
-	rightValue := inOrderTraversalResult[len(inOrderTraversalResult) - 1]
+	return rebuild(
+		preOrderTraversalResult,
+		0,
+		len(preOrderTraversalResult)-1,
+		inOrderTraversalResult,
+		0,
+		len(inOrderTraversalResult)-1,
+	)
+}
 
-	leftNode := Node{
-		left: nil,
-		right: nil,
-		value: leftValue,
+func SliceIndex(nums []int, predicate int) int {
+	for i, num := range nums {
+		if num == predicate {
+			return i
+		}
 	}
-	rightNode := Node{
-		left: nil,
+	return -1
+}
+
+func rebuild(preOrderTraversalResult []int, preOrderStartIndex int, preOrderEndIndex int,
+	inOrderTraversalResult []int, inOrderStartIndex int, inOrderEndIndex int) (rootNode *Node) {
+
+	rootValue := preOrderTraversalResult[preOrderStartIndex]
+	rootNode = &Node{
+		left:  nil,
 		right: nil,
-		value: rightValue,
-	}
-	rootNode := Node{
-		left: &leftNode,
-		right: &rightNode,
 		value: rootValue,
 	}
-	return &rootNode
+
+	// 直接返回只有一个节点的树
+	if preOrderStartIndex == preOrderEndIndex {
+		return
+	}
+
+	rootIndexOfInOrderTraversalResult := SliceIndex(inOrderTraversalResult, rootValue)
+	leftTreeNodeNum := rootIndexOfInOrderTraversalResult - inOrderStartIndex
+	leftPreOrderEndIndex := preOrderStartIndex + leftTreeNodeNum
+
+	if leftTreeNodeNum > 0 {
+		rootNode.left = rebuild(
+			preOrderTraversalResult,
+			preOrderStartIndex+1, // 排除当前 root 节点
+			leftPreOrderEndIndex,
+			inOrderTraversalResult,
+			inOrderStartIndex,
+			rootIndexOfInOrderTraversalResult-1,
+		)
+	}
+
+	nonRootNodeNum := preOrderEndIndex - preOrderStartIndex
+	rightTreeNodeNum := nonRootNodeNum - leftTreeNodeNum
+	if rightTreeNodeNum > 0 {
+		rootNode.right = rebuild(
+			preOrderTraversalResult,
+			leftPreOrderEndIndex+1,
+			preOrderEndIndex,
+			inOrderTraversalResult,
+			rootIndexOfInOrderTraversalResult+1,
+			inOrderEndIndex,
+		)
+	}
+
+	return
 }

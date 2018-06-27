d514d5ca9861ebbc0c056e4a06108ee9bbdfa910
揭思敏 <jsm0834@175game.com>
2018-06-27 09:16:05 +0800

    007-用两个栈实现队列: v2: feat: 增加 Queue.dequeue()

--- 007-用两个栈实现队列/v2/queue.go
+++ 007-用两个栈实现队列/v2/queue.go
@@ -7,3 +7,20 @@ type Queue struct {
 func (queue *Queue) enqueue(i int) {
 	queue.Data = append(queue.Data, i)
 }
+
+func (queue *Queue) dequeue() (result int) {
+	if len(queue.Data) == 0 {
+		return
+	}
+
+	result = queue.Data[0]
+	newStartIndex := 1
+	if newStartIndex == len(queue.Data) {
+		queue.Data = []int{}
+	} else {
+		queue.Data = queue.Data[newStartIndex:]
+	}
+
+	return
+
+}
--- 007-用两个栈实现队列/v2/queue_test.go
+++ 007-用两个栈实现队列/v2/queue_test.go
@@ -19,5 +19,18 @@ func TestQueue(t *testing.T) {
 			t.Errorf("got %v want %v", got, want)
 		}
 	})
-	
+
+	t.Run("enqueue", func(t *testing.T) {
+		queue := Queue{[]int{1, 2}}
+
+		got := []int{
+			queue.dequeue(),
+			queue.dequeue(),
+		}
+		want := []int{1, 2}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
 }

806f1251315b500ce58730edf68579b71496ebf0
揭思敏 <jsm0834@175game.com>
2018-06-30 21:41:24 +0800

    008-旋转数组的最小数字: v1: feat: 处理空 array 的情况

--- 008-旋转数组的最小数字/v1/rotate_array.go
+++ 008-旋转数组的最小数字/v1/rotate_array.go
@@ -2,6 +2,11 @@ package main
 
 func RotateArray(array []int, k int) []int {
 	arrayLength := len(array)
+
+	if arrayLength == 0 {
+		return array
+	}
+
 	for i := 0; i < k; i ++ {
 		array = append([]int{array[arrayLength - 1]}, array[:arrayLength-1]...)
 	}
--- 008-旋转数组的最小数字/v1/rotate_array_test.go
+++ 008-旋转数组的最小数字/v1/rotate_array_test.go
@@ -6,12 +6,26 @@ import (
 )
 
 func TestRotateArray(t *testing.T) {
-	array := []int{1, 2, 3, 4, 5, 6, 7}
-	k := 3
-	got := RotateArray(array, k)
-	want := []int{5, 6, 7, 1, 2, 3, 4}
+	t.Run("array's length greater than k", func(t *testing.T) {
+		array := []int{1, 2, 3, 4, 5, 6, 7}
+		k := 3
+		got := RotateArray(array, k)
+		want := []int{5, 6, 7, 1, 2, 3, 4}
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("empty array", func(t *testing.T) {
+		var array []int
+
+		k := 3
+		got := RotateArray(array, k)
+		var want []int
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }

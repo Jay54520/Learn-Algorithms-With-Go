ac1ab11143a495d12dbcc93d3fe8ed4f23bd3c19
揭思敏 <jsm0834@175game.com>
2018-07-01 09:57:52 +0800

    009-斐波那契数列: v1: feat: Fib normal condition

--- 009-斐波那契数列/v1/fib.go
+++ 009-斐波那契数列/v1/fib.go
@@ -3,7 +3,8 @@ package main
 func Fib(i int) int {
 	if i == 0 || i == 1 {
 		return i
+	} else {
+		return Fib(i - 1) + Fib(i - 2)
 	}
-	return 0
 }
 
--- 009-斐波那契数列/v1/fib_test.go
+++ 009-斐波那契数列/v1/fib_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestFib(t *testing.T)  {
+func TestFib(t *testing.T) {
 	t.Run("base condition", func(t *testing.T) {
 		got := Fib(0)
 		want := 0
@@ -16,6 +16,15 @@ func TestFib(t *testing.T)  {
 
 		if got1 != want1 {
 			t.Errorf("got %d want %d", got1, want1)
+		}
+	})
+
+	t.Run("normal condition", func(t *testing.T) {
+		got := Fib(3)
+		want := 2
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
 		}
 	})
 }

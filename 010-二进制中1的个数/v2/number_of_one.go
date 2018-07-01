249936331adc1763d56be38057cb60c4a921c3f4
揭思敏 <jsm0834@175game.com>
2018-07-01 16:24:57 +0800

    010-二进制中1的个数: v2: refactor: 使用位操作遍历

--- 010-二进制中1的个数/v2/number_of_one.go
+++ 010-二进制中1的个数/v2/number_of_one.go
@@ -1,15 +1,18 @@
 package main
 
-import (
-	"strconv"
-	"strings"
-)
-
-const BINARY = 2
-
 // NumberOfOne 输出输入参数二进制表示的 1 的个数
-func NumberOfOne(i int64) int {
-	binaryString := strconv.FormatInt(i, BINARY)
-	return strings.Count(binaryString, "1")
-}
+// flag 只有一位是 1，其他位是 0，所以如果 i 的对应为不是 1，
+// 那么 i & flag 为 0
+// flag 越界后变为 0，说明遍历完了
+func NumberOfOne(i int64) (count int) {
+	var flag int64 = 1
 
+	for flag != 0 {
+		if i&flag != 0 {
+			count += 1
+		}
+		flag <<= 1
+	}
+
+	return
+}

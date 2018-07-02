b38d853cffaeb84a69d4c395d3dbd2606a12f1d8
揭思敏 <jsm0834@175game.com>
2018-07-02 09:11:12 +0800

    011-数值的整数次方: v1: test: add test "exponent == 0"

--- 011-数值的整数次方/v1/calculate_exponent_test.go
+++ 011-数值的整数次方/v1/calculate_exponent_test.go
@@ -3,12 +3,25 @@ package main
 import "testing"
 
 func TestCalculateExponent(t *testing.T)  {
-	var base float64 = 2.2
-	exponent := 3
-	got := CalculateExponent(base, exponent)
-	want := 10.648000000000003
+	t.Run("exponent > 0", func(t *testing.T) {
+		var base float64 = 2.2
+		exponent := 3
+		got := CalculateExponent(base, exponent)
+		want := 10.648000000000003
 
-	if got != want {
-		t.Errorf("got %v want %v", got, want)
-	}
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("exponent == 0", func(t *testing.T) {
+		var base float64 = 2.2
+		exponent := 0
+		got := CalculateExponent(base, exponent)
+		want := 1.0
+
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }

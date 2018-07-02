2d8c8b227f737c195e7f3974d2c307591a9e99ba
揭思敏 <jsm0834@175game.com>
2018-07-02 09:21:46 +0800

    011-数值的整数次方: v1: feat: 处理 exponent 小于 0

--- 011-数值的整数次方/v1/calculate_exponent.go
+++ 011-数值的整数次方/v1/calculate_exponent.go
@@ -1,9 +1,24 @@
 package main
 
-func CalculateExponent(base float64, exponent int) float64 {
-	var result float64 = 1
-	for i := 0; i < exponent; i++ {
+import (
+	"math"
+	"errors"
+)
+
+func CalculateExponent(base float64, exponent int) (result float64,  err error) {
+	result = 1
+
+	for i := 0; i < int(math.Abs(float64(exponent))); i++ {
 		result *= base
 	}
-	return result
+
+	if exponent < 0 {
+
+		if base == 0 {
+			err = errors.New("base 不能为 0 当 exponent 为负数")
+		}
+		result = 1 / result
+	}
+
+	return
 }
--- 011-数值的整数次方/v1/calculate_exponent_test.go
+++ 011-数值的整数次方/v1/calculate_exponent_test.go
@@ -6,7 +6,7 @@ func TestCalculateExponent(t *testing.T)  {
 	t.Run("exponent > 0", func(t *testing.T) {
 		var base float64 = 2.2
 		exponent := 3
-		got := CalculateExponent(base, exponent)
+		got, _ := CalculateExponent(base, exponent)
 		want := 10.648000000000003
 
 		if got != want {
@@ -17,11 +17,33 @@ func TestCalculateExponent(t *testing.T)  {
 	t.Run("exponent == 0", func(t *testing.T) {
 		var base float64 = 2.2
 		exponent := 0
-		got := CalculateExponent(base, exponent)
+		got, _ := CalculateExponent(base, exponent)
 		want := 1.0
 
 		if got != want {
 			t.Errorf("got %v want %v", got, want)
 		}
 	})
+
+	t.Run("exponent < 0 && base != 0", func(t *testing.T) {
+		var base float64 = 2.2
+		exponent := -3
+		got, _ := CalculateExponent(base, exponent)
+		want := 1.0 / 10.648000000000003
+
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("exponent < 0 && base == 0", func(t *testing.T) {
+		var base float64 = 0
+		exponent := -3
+		_, err := CalculateExponent(base, exponent)
+
+		if err == nil {
+			t.Errorf("err %v shouldn't be nil", err)
+		}
+	})
+
 }

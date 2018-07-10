40f6ad01fec3abe8b7b009464279b4baece60c62
揭思敏 <jsm0834@175game.com>
2018-07-10 09:09:15 +0800

    025-二叉树中和为某一值的路径: v2: develop: 改变为全局变量后还是如此
    说明不是 *paths 造成的

--- 025-二叉树中和为某一值的路径/v2/k_sum_paths.go
+++ 025-二叉树中和为某一值的路径/v2/k_sum_paths.go
@@ -5,48 +5,50 @@ import (
 	"fmt"
 )
 
+var globalPaths [][]int
+
 // KSumPaths 题意见 https://www.geeksforgeeks.org/print-k-sum-paths-binary-tree/
 // 相比于 KSumPathsFromRoot，这里变为对所有节点求 KSumPathsFromRoot
 func KSumPaths(root *Node, k int) (paths [][]int) {
 	if root == nil {
 		return
 	} else {
-		kSumPathsFromRoot(root, k, &paths)
+		kSumPathsFromRoot(root, k)
 
 		if root.left != nil {
-			kSumPathsFromRoot(root.left, k, &paths)
+			kSumPathsFromRoot(root.left, k)
 		}
 
 		if root.right != nil {
-			kSumPathsFromRoot(root.right, k, &paths)
+			kSumPathsFromRoot(root.right, k)
 		}
 
-		return
+		return globalPaths
 	}
 
 }
 
-func kSumPathsFromRoot(root *Node, k int, tmpPaths *[][]int) {
+func kSumPathsFromRoot(root *Node, k int) {
 	if root == nil {
 		return
 	} else {
 		path := []int{root.value}
-		search(k, tmpPaths, path, root.left)
-		search(k, tmpPaths, path, root.right)
+		search(k, path, root.left)
+		search(k, path, root.right)
 	}
 
 	if root.left != nil {
-		kSumPathsFromRoot(root.left, k, tmpPaths)
+		kSumPathsFromRoot(root.left, k)
 	}
 	if root.right != nil {
-		kSumPathsFromRoot(root.right, k, tmpPaths)
+		kSumPathsFromRoot(root.right, k)
 	}
 }
 
-func search(k int, paths *[][]int, path []int, root *Node) {
+func search(k int, path []int, root *Node) {
 
 	hasSamePath := false
-	for _, existedPath := range *paths {
+	for _, existedPath := range globalPaths {
 		// 不统计已存在的路径，由于左右节点为空会造成相同的路径
 		if reflect.DeepEqual(existedPath, path) {
 			hasSamePath = true
@@ -57,9 +59,9 @@ func search(k int, paths *[][]int, path []int, root *Node) {
 	if !hasSamePath {
 		sum := sumSlice(path)
 		if sum == k {
-			fmt.Printf("Current paths: %v \n", *paths)
-			*paths = append(*paths, path)
-			fmt.Printf("Current paths: %v \n", *paths)
+			fmt.Printf("Current paths: %v \n", globalPaths)
+			globalPaths = append(globalPaths, path)
+			fmt.Printf("Current paths: %v \n", globalPaths)
 		}
 	}
 
@@ -67,8 +69,8 @@ func search(k int, paths *[][]int, path []int, root *Node) {
 		return
 	} else {
 		path = append(path, root.value)
-		search(k, paths, path, root.left)
-		search(k, paths, path, root.right)
+		search(k, path, root.left)
+		search(k, path, root.right)
 	}
 
 }

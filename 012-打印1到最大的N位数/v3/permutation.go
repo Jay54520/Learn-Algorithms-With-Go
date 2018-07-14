eef62722bbb60c645eed9ffb3b396754e18c141c
揭思敏 <jsm0834@175game.com>
2018-07-14 15:15:47 +0800

    012-打印1到最大的N位数: v3: 改进注释。
    将 Permutation 的注释移动到 permutation。

--- 012-打印1到最大的N位数/v3/permutation.go
+++ 012-打印1到最大的N位数/v3/permutation.go
@@ -5,16 +5,17 @@ import (
 	"strings"
 )
 
-// Permutation 采用递归实现
-// 递归循环：当前序列的全排列 = 某一个元素加上剩余元素的全排列。如果让第一个元素与其他
-// 剩余元素依次交换位置，那么全排列就 =（交换后的）第一个元素加上剩余元素的全排列
-// 递归终止条件：为当前序列中只有一个元素，因为一个元素的全排列就是它本身。这时的 stringSlice 就是
-// 一组完整的排列结果
+// Permutation 返回 stringSlice 的全排列
 func Permutation(stringSlice []string) (resultSlice []string) {
 	permutation(stringSlice, 0, len(stringSlice)-1, &resultSlice)
 	return
 }
 
+// permutation 采用递归实现
+// 递归循环：当前序列的全排列 = 某一个元素加上剩余元素的全排列。如果让第一个元素与其他
+// 剩余元素依次交换位置，那么全排列就 =（交换后的）第一个元素加上剩余元素的全排列
+// 递归终止条件：为当前序列中只有一个元素，因为一个元素的全排列就是它本身。这时的 stringSlice 就是
+// 一组完整的排列结果
 func permutation(stringSlice []string, startIndex, endIndex int, resultSlice *[]string)  {
 	if startIndex == endIndex {
 		result := fmt.Sprint(strings.Join(stringSlice, ""))

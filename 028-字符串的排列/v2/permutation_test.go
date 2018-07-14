65a6f4eb5ff92e581502a5015ee09b84a62452f9
揭思敏 <jsm0834@175game.com>
2018-07-14 15:53:42 +0800

    028-字符串的排列: v2: test: add test "has same characters"

---
+++ 028-字符串的排列/v2/permutation_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"testing"
+	"reflect"
+)
+
+func TestPermutation(t *testing.T) {
+	t.Run("unique characters", func(t *testing.T) {
+		got := Permutation("abc")
+		want := []string{"abc", "acb", "bac", "bca", "cab", "cba"}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("has same characters", func(t *testing.T) {
+		got := Permutation("aba")
+		want := []string{"aab", "aba", "baa"}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+}

71d5abe84e83e0efaca4170d5163332432987295
揭思敏 <jsm0834@175game.com>
2018-07-15 21:26:18 +0800

    030-最小的K个数: v1: refactor: 使用 sort.SearchInts 重构
    sort.SearchInts 返回数字需要插入到的索引，相当于写了删除代码的逻辑。

--- 030-最小的K个数/v1/insert_to_sorted_slice.go
+++ 030-最小的K个数/v1/insert_to_sorted_slice.go
@@ -4,9 +4,7 @@ import "sort"
 
 // InsertToSortedSlice 将 number 插入 sortedSLice 并使插入后的
 // sortedSLice 仍然有序
-// 使用二分搜索在 sortedSLice 中查找大于等于 number 的第一个索引，
-// 如果找到了，将 number 插入到这个索引对应的数字前面；
-// 否则，说明 number 是大于 sortedSLice 中的所有数，将 number 插入到最后
+// 使用sort.SearchInts（二分搜索）查找插入的索引，将 number 插入到这个索引位置
 func InsertToSortedSlice(sortedSLice *[]int, number int) {
 	sortedSliceValue := *sortedSLice
 	var index int
@@ -16,29 +14,18 @@ func InsertToSortedSlice(sortedSLice *[]int, number int) {
 		*sortedSLice = sortedSliceValue
 		return
 	} else {
-		index = sort.Search(number, func(i int) bool {
-			if i < len(sortedSliceValue) && sortedSliceValue[i] >= number {
-				return true
-			} else {
-				return false
-			}
-		})
+		index = sort.SearchInts(sortedSliceValue, number)
 	}
 
-	// 没有找到大于等于 number 的数
-	if index == number {
-		*sortedSLice = append(sortedSliceValue, number)
-	} else {
-		// ------------将 number 插入到 index 这个位置------------
-		// 参考 https://gist.github.com/zhum/57cb45d8bbea86d87490
-		// 扩大 cap，将 index 和之后的所有元素复制到 index + 1 之后，然后替换 index 的值
-		if len(sortedSliceValue) == cap(sortedSliceValue) {
-			sortedSliceValue = append(sortedSliceValue, 0)
-		}
-		copy(sortedSliceValue[index+1:], sortedSliceValue[index:])
-		sortedSliceValue[index] = number
-		// ------------将 number 插入到 index 这个位置------------
-
-		*sortedSLice = sortedSliceValue
+	// ------------将 number 插入到 index 这个位置------------
+	// 参考 https://gist.github.com/zhum/57cb45d8bbea86d87490
+	// 扩大 cap，将 index 和之后的所有元素复制到 index + 1 之后，然后替换 index 的值
+	if len(sortedSliceValue) == cap(sortedSliceValue) {
+		sortedSliceValue = append(sortedSliceValue, 0)
 	}
+	copy(sortedSliceValue[index+1:], sortedSliceValue[index:])
+	sortedSliceValue[index] = number
+	// ------------将 number 插入到 index 这个位置------------
+
+	*sortedSLice = sortedSliceValue
 }

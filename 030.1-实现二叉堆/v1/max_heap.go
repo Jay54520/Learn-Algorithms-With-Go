d2f3839a7561102e71cc3758b9e080e6f0986c01
揭思敏 <jsm0834@175game.com>
2018-07-18 20:59:23 +0800

    030.1-实现二叉堆: v1: refactor: 重构 Push
    推理出上浮的原理并提高代码可读性

--- 030.1-实现二叉堆/v1/max_heap.go
+++ 030.1-实现二叉堆/v1/max_heap.go
@@ -1,42 +1,46 @@
 package main
 
-const rootIndex = 1
-const placeHolder = 0
+const rootIndex = 0
 
 // 使用数组实现最大堆，并含有 Push 和 Pop 方法
 type MaxIntHeap []int
 
-// 将 i 添加到数组末尾，然后使其上浮（与父节点交换）到合适位置。以索引 1 为
-// 根节点（第一个节点），i 表示第 i 个节点，由于二叉堆是完全二叉树，所以左右子节点（如果有的话）
-// 分别为 2i、2i + 1。最大堆的关系是父节点大于两个左右子节点，以下简称关系。
-// 添加一个节点后，如果添加节点不满足关系，那么上浮它
-// 终止条件：满足关系或者没有父节点了（它变成了根节点）
-func (h *MaxIntHeap) Push(newNum int) {
-	if len(*h) == 0 {
-		*h = append(*h, placeHolder)
-	}
+// 将 i 添加到数组末尾，然后使其上浮（与父节点交换）到合适位置。
+// 2i + 1, 2i + 2 分别为 i 节点的左右子节点，比如索引分别为 0 1 2 3 4 5 6。
+// i 的父节点为 (i - 1) /2。因为 (2i +1 - 1)/2 = i 和 (2i + 2 - 1) / 2 = i
+// 最大堆的关系是父节点大于两个左右子节点，左右节点之间没有大小关系，以下简称关系。
+//
+// 添加一个节点后，新增节点是一个叶子节点。如果新增节点小于它的父节点，那么符合关系，返回；
+//否则，新增节点大于它的父节点。由于添加节点前二叉堆处于有序状态，所以如果父节点存在一个左子节点，
+//那么父节点大于左子节点，因此新增节点也大于左子节点。将新增节点与父节点互换后，以新增节点为根节点的
+//树符合关系。同理，新增节点再次与它的父节点比较并决定返回或是交换。如果新增节点变成了根节点，那么以
+//新增节点为根节点的树就是整棵树，即整棵树符合关系了。
+//
+// 将节点添加到末尾，然后
+// 循环代码为：查看新增节点是否满足终止条件，如果满足，则返回；否则将新增节点与其父节点交换
+// 终止条件：新增节点变成了根节点或者新增节点小于它的父节点
+func (h *MaxIntHeap) Push(element int) {
+	var parentIndex int
+	*h = append(*h, element)
+	elementIndex := len(*h) - 1
+	shouldStopUp := h.shouldStopUp(elementIndex)
 
-	*h = append(*h, newNum)
-	newNumIndex := len(*h) - 1
-	relation := h.getRelation(newNumIndex)
-	for !relation {
-		parentIndex := newNumIndex / 2
-		(*h)[parentIndex], (*h)[newNumIndex] = (*h)[newNumIndex], (*h)[parentIndex]
-		relation = h.getRelation(parentIndex)
+	for !shouldStopUp {
+		parentIndex = (elementIndex - 1) / 2
+		(*h)[parentIndex], (*h)[elementIndex] = (*h)[elementIndex], (*h)[parentIndex]
+		elementIndex = parentIndex
+		shouldStopUp = h.shouldStopUp(elementIndex)
 	}
 }
 
-// 是否满足关系或没有父节点
-func (h *MaxIntHeap) getRelation(index int) bool {
+func (h *MaxIntHeap) shouldStopUp(index int) (shouldStopUp bool) {
+	// 因为根节点没有父节点，所以要先判断这个
 	if index == rootIndex {
-		return true
+		shouldStopUp = true
 	}
-
-	parentIndex := index / 2
-	// 父节点大于子节点
-	if (*h)[parentIndex] > (*h)[index] {
-		return true
-	} else {
-		return false
+	parentIndex := (index - 1) / 2
+	if (*h)[index] < (*h)[parentIndex] {
+		shouldStopUp = true
 	}
+	return
 }
--- 030.1-实现二叉堆/v1/max_heap_test.go
+++ 030.1-实现二叉堆/v1/max_heap_test.go
@@ -10,7 +10,7 @@ func TestMaxIntHeap_Push(t *testing.T) {
 	h.Push(1)
 	h.Push(2)
 	h.Push(3)
-	want := MaxIntHeap{placeHolder, 3, 1, 2}
+	want := MaxIntHeap{3, 1, 2}
 	if !reflect.DeepEqual(*h, want) {
 		t.Errorf("got %v want %v", *h, want)
 	}
